84a205854340d2cc8ad7de1373955e51ad82a570
Luiz-Rodrigues <luizrodrigues090705@gmail.com>
2025-02-04 16:00:25 -0300

    style: adicionando favicon e removendo projeto.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,12 +16,12 @@ export const projects: Array<ProjectType> = [
     link: "https://minerva-gamma.vercel.app",
   },
   {
-    title: "Portfólio",
+    title: "Desenvolvimento",
     description:
-      "Aplicação WEB que expõe dados públicos de usuários do GitHub, através de sua API. Esse foi o primeiro projeto em que consumi uma API.",
-    libraries: ["ReactJS", "Typescript", "CSS", "HTML"],
-    image: "/images/portfolio-01.jpg",
-    link: "#hero",
+      "Lorem ipsum dolor sit amet. Aut similique vero non consequatur malaquitus eaque vel molestias esse. Hic exercitationem.",
+    libraries: ["Em", "Breve", "Em", "Breve"],
+    image: "/images/foto-desenvolvimento.jpg",
+    link: "#projects",
   },
   {
     title: "Desenvolvimento",
